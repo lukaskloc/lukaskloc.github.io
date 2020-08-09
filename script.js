6896d21f84a92d12ec38fea59a023fb3f1045f73
lukaskloc <lukas.kloc@gmail.com>
2020-08-09 21:28:14 +0200

    todays

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
   var apiRoot = 'http://powerful-lake-74696.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://powerful-lake-74696.herokuapp.com/v1/trello';
+  const trelloApiRoot = 'http://powerful-lake-74696.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
